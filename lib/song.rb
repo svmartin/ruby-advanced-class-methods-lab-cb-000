e904cec43c6935d97fa9db3852d1a8df702ff35a
Sean Martin <sean.martin@gmail.com>
2018-10-11 18:52:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -43,8 +43,7 @@ class Song
 
   def self.new_from_filename(filename)
     song = self.new
-    song.name, song.artist_name = filename.split("-")
-    song
+    song.artist_name, song.name =
   end
 
   def self.create_from_filename(filename)
