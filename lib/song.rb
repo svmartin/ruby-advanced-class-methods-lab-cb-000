133de87c413f9a22c22e8d3613d48ad965f737dd
Sean Martin <sean.martin@gmail.com>
2018-10-11 18:32:21 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,25 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create()
+    song = self.new
+    self.class.all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    self.class.all << song
+    song
+  end
+
   def self.all
     @@all
   end
