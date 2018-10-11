b4f254f7d39072214a49fecd6f6c941330542d6a
Sean Martin <sean.martin@gmail.com>
2018-10-11 18:39:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,11 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    #
+    if self.find_by_name(name) != nil
+      self.find_by_name(name)
+    else
+      self.create_by_name(name)
+    end
   end
 
   def self.all
